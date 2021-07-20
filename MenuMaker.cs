1709d815fa8e241ed33cc2c5a2ce411c512cda2d
Joanna <aska3001@wp.pl>
2021-07-20 20:36:29 +0200

    Add project files.

---
+++ MenuMaker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu
+{
+    class MenuMaker
+    {
+        public Random Randomizer;
+
+        string[] Meats = { "Roast beef", "Salami", "Turkey", "Ham", "Pastrami" };
+        string[] Condiments = { "yellow mustard", "brown mustard", "honey mustard", "mayonnnaise", "relish", "french dressing" };
+        string[] Breads = { "rice bread", "white bread", "wheat bread", "pumpernickel", "italian bread", "a roll" };
+
+        public string GetMenuItem()
+        {
+            string randomMeat = Meats[Randomizer.Next(Meats.Length)];
+            string randomCondiment = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBread = Breads[Randomizer.Next(Breads.Length)];
+            return randomMeat + ", " + randomCondiment + ", " + randomBread;
+        }
+
+    }
+}
